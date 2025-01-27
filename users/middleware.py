ce6c8c71395efaf560136b8d793c83031acd8272
Adilnur1 <osmonaliievv@gmail.com>
2025-01-27 18:10:18 +0600

    register fixed

--- users/middleware.py
+++ users/middleware.py
@@ -6,15 +6,19 @@ class ExperienceMiddleWare(MiddlewareMixin):
     def process_request(self, request):
         if request.path == '/register/' and request.method == 'POST':
             experience = request.POST.get('experience')
-            if experience:
-                experience = int(experience)
-                if 1 <= experience <= 3:
-                    return HttpResponseBadRequest('Ваша зарплата составляет 1000$')
-                elif 3 < experience <= 7:
-                    return HttpResponseBadRequest('Ваша зарплата составляет 2000$')
-                elif 7 < experience <= 10:
-                    return HttpResponseBadRequest('Ваша зарплата составляет 5000$')
-                elif experience <= 0:
-                    return HttpResponseBadRequest('Ваш опыт работы не соответствует требованиям')
+            if not experience or not experience.isdigit():
+                return HttpResponseBadRequest('Некорректное значение опыта работы.')
+
+            experience = int(experience)
+            if experience <= 0:
+                return HttpResponseBadRequest('Ваш опыт работы не соответствует требованиям.')
+            elif 1 <= experience <= 3:
+                request.salary = 'Ваша зарплата составляет 1000$'
+            elif 3 < experience <= 7:
+                request.salary = 'Ваша зарплата составляет 2000$'
+            elif 7 < experience <= 10:
+                request.salary = 'Ваша зарплата составляет 5000$'
+            else:
+                request.salary = 'Опыт работы не соответствует требованиям.'
         elif request.path == '/register/' and request.method == 'GET':
             setattr(request, 'salary', 'Зарплата не указана')
